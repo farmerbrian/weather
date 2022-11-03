a4baeb837d390f0fde325689d399692b92f8353a
farmerbrian <nichols.brian.d@gmail.com>
2022-11-03 09:55:59 -0500

    Added more styling and basic error handling

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const img = document.querySelector('img');
 const form = document.querySelector('form');
 const searchEl = document.getElementById('search');
-const pEl = document.getElementById('log');
+const toggle = document.getElementById('temp-toggle');
 const apiKey = 'c715646b312c3dbc0c7e222279cd0de4';
-const units = 'imperial';
+let units = '';
 const weatherObj = {
 	name: '',
 	feelsLike: '',
@@ -11,30 +11,48 @@ const weatherObj = {
 	tempHigh: '',
 	tempLow: '',
 	conditions: '',
+	icon: '',
 };
 let weatherData = {};
+
+function setUnits() {
+	if (toggle.checked == false) {
+		units = 'imperial';
+	} else {
+		units = 'metric';
+	}
+}
 
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
 	weatherCall();
 });
 
-async function weatherCall() {
-	const weatherResponse = await fetch(
-		`https://api.openweathermap.org/data/2.5/weather?q=${searchEl.value}&units=${units}&APPID=${apiKey}`,
-		{ mode: 'cors' }
-	);
-	weatherData = await weatherResponse.json();
-	weatherObj.temp = parseInt(weatherData.main.temp);
-	weatherObj.name = weatherData.name;
-	weatherObj.feelsLike = parseInt(weatherData.main.feels_like);
-	weatherObj.tempHigh = parseInt(weatherData.main.temp_max);
-	weatherObj.tempLow = parseInt(weatherData.main.temp_min);
-	weatherObj.conditions = weatherData.weather[0].main;
+toggle.addEventListener('click', (event) => {
+	setUnits();
+	weatherCall();
+});
 
-	// pEl.innerHTML = `The current temperature for ${weatherData.name} is ${weatherObj.temp}&#176`;
-	console.log(weatherObj);
-	displayWeather();
+async function weatherCall() {
+	setUnits();
+	try {
+		const weatherResponse = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${searchEl.value}&units=${units}&APPID=${apiKey}`,
+			{ mode: 'cors' }
+		);
+		weatherData = await weatherResponse.json();
+		weatherObj.temp = parseInt(weatherData.main.temp);
+		weatherObj.name = weatherData.name;
+		weatherObj.feelsLike = parseInt(weatherData.main.feels_like);
+		weatherObj.tempHigh = parseInt(weatherData.main.temp_max);
+		weatherObj.tempLow = parseInt(weatherData.main.temp_min);
+		weatherObj.conditions = weatherData.weather[0].description;
+		weatherObj.icon = weatherData.weather[0].icon;
+		displayWeather();
+	} catch (error) {
+		const container = document.getElementById('weather-container');
+		container.innerHTML = 'ERROR - Please enter a valid city name.';
+	}
 }
 
 function displayWeather() {
@@ -44,37 +62,14 @@ function displayWeather() {
 	const h3CurrentTemp = document.getElementById('current-temp');
 	const h3High = document.getElementById('high-temp');
 	const h3Low = document.getElementById('low-temp');
+	const conditionsIcon = document.getElementById('conditions-icon');
 
 	pName.innerHTML = `${weatherObj.name}`;
-	pTemp.innerHTML = `${weatherObj.feelsLike}`;
-	h3Conditions.innerHTML = `${weatherObj.conditions}`;
-	h3CurrentTemp.innerHTML = `${weatherObj.temp}`;
-	h3High.innerHTML = `${weatherObj.tempHigh}`;
-	h3Low.innerHTML = `${weatherObj.tempLow}`;
+	pTemp.innerHTML = `${weatherObj.feelsLike}&#176`;
+	h3CurrentTemp.innerHTML = `Current Temperature: ${weatherObj.temp}&#176`;
+	h3High.innerHTML = `High for the day: ${weatherObj.tempHigh}&#176`;
+	h3Low.innerHTML = `Low for the day: ${weatherObj.tempLow}&#176`;
+	conditionsIcon.src = `http://openweathermap.org/img/wn/${weatherObj.icon}.png`;
 }
 
 weatherCall();
-
-// searchEl.addEventListener('keyup', gifSearch);
-// console.log(searchEl.value);
-
-// function gifSearch() {
-// 	if (!searchEl.value) {
-// 		console.log('false');
-// 		gifCall('loser');
-// 	} else {
-// 		console.log('true');
-// 		gifCall(searchEl.value);
-// 	}
-// }
-
-// async function gifCall(input) {
-// 	const apiResponse = await fetch(
-// 		`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${input}`,
-// 		{ mode: 'cors' }
-// 	);
-// 	const apiData = await apiResponse.json();
-// 	img.src = apiData.data.images.original.url;
-
-// }
-// gifSearch();
